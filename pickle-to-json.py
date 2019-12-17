c7004b819b0a761d51d634f24c2c41c8a72de453
Stan Nedev <stan@nedev.io>
2019-12-17 11:18:48 +0200

    Main file upload

---
+++ pickle-to-json.py
@@ -0,0 +1,30 @@
+import pickle
+import pandas as pd
+from sys import argv
+
+script, filename = argv
+
+# Load the pickle format file
+input_file = open(filename, 'rb')
+new_dict = pickle.load(input_file)
+input_file()
+
+# Create a Pandas DataFrame
+df = pd.DataFrame(new_dict)
+
+# Copy DataFrame index as a column
+df['index1'] = df.index
+
+# Move the new index column to the from of the DataFrame
+index1 = df['index1']
+df.drop(labels=['index1'], axis=1,inplace = True)
+df.insert(0, 'index1', index1)
+
+# Convert to json values
+json_df = df.to_json(orient='values', date_format='iso', date_unit='s')
+
+# Create and record the JSON data in a new .JSON file
+with open('data.json', 'w') as js_file:
+    js_file.write(json_df)
+
+
